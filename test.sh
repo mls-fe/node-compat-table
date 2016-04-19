3a56d4f1cc3214be462c557224a3a2aab8f1922e
sunnylost <sunnylost@gmail.com>
2016-04-19 12:46:23 +0800

    fix test.sh

--- test.sh
+++ test.sh
@@ -11,12 +11,6 @@ while read v; do
   n use $v --es_staging test.js
 done < .versions
 
-
-LATEST=$(curl -sL https://nodejs.org/download/nightly/index.tab |   awk '{ if (!f && NR > 1) { print $1; f = 1 } }')
-PROJECT_NAME="node" PROJECT_URL="https://nodejs.org/download/nightly/" n project $LATEST
-node test.js
-node --es_staging test.js
-
 echo
 echo 'building webpage...'
 node build.js
